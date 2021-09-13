ae041a5b49506ee32c7e31e9a95c80399a49df1a
jajo21 <89775394+jajo21@users.noreply.github.com>
2021-09-13 10:01:44 +0200

    Har lagt till clickfunktion och loginfunktion så att man tas till välkomstsida eller till felmeddelandesida

--- script.js
+++ script.js
@@ -6,3 +6,37 @@ const username = document.querySelector("#username");
 const password = document.querySelector("#password");
 const logInBtn = document.querySelector("#logInBtn");
 
+/* Skapar en function som ska utföra hämtningen av inputs + utföra inloggingen,
+ samt sparande i localStorage till slut */
+logInBtn.addEventListener("click", function(){
+    const usernameInput = username.value;
+    const passwordInput = password.value;
+    let user = false;
+    if(usernameInput == "test" && passwordInput == "1234") {
+        user = true;
+        localStorage.setItem(usernameInput, passwordInput);
+        logIn(user);
+    } else {
+        user = false;
+        logIn(user);
+    }
+});
+
+function logIn(user) {
+    if (user == true) {
+        meny.textContent = "";
+        const welcome = document.createElement("h2")
+        welcome.innerText = "Välkommen in i värmen";
+        meny.appendChild(welcome);
+    }
+    else {
+        meny.textContent = "";
+        const welcome = document.createElement("h2")
+        welcome.innerText = "Du har tyvärr skrivit fel användarnamn eller lösenord";
+        meny.appendChild(welcome);
+    }
+}
+
+/*function logOut() {
+    localStorage
+}*/
