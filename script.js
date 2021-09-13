a99f3ad84c8015a3d5ab26b6f5209d96671670d8
jajo21 <89775394+jajo21@users.noreply.github.com>
2021-09-13 09:24:39 +0200

    Ändrade javascript source i html filen samt la till variabler i js filen

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const namn = "test";
 const lösenord = "1234";
+
+const meny = document.querySelector(".meny");
+const username = document.querySelector("#username");
+const password = document.querySelector("#password");
+const logInBtn = document.querySelector("#logInBtn");
+
